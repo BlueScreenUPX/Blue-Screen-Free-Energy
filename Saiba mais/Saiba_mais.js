e914ad02c37e9d51248f06428722d3a12f87dc2a
GustavoGaldino <gustavo.galdino.2005@gmail.com>
2024-10-21 02:03:51 -0300

    Free Energy V.0.1.8 Adicionando a Pasta Global para adicionar configurações que serão usadas em todas as paginas, adicionando Global.Css para o codigo da fonte, Adicionando tambem o JavaScript global para para a animação das paginas, excluindo os JavaScripts individuais desnecessarios, Excluindo a pasta img e movendo as imagens para assets/img.

--- Saiba mais/Saiba_mais.js
+++ Saiba mais/Saiba_mais.js
@@ -22,20 +22,20 @@ document.querySelectorAll('.menu-link a').forEach(link => {
 
 
 
-// Animação aparecendo
-document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll('.fade-in');
+// // Animação aparecendo
+// document.addEventListener("DOMContentLoaded", function () {
+//     const elements = document.querySelectorAll('.fade-in');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    });
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add('visible');
+//                 observer.unobserve(entry.target);
+//             }
+//         });
+//     });
 
-    elements.forEach(element => {
-        observer.observe(element);
-    });
-});
+//     elements.forEach(element => {
+//         observer.observe(element);
+//     });
+// });
